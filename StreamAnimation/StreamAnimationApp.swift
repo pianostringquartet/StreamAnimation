456fcd33e3bc09c933096ceef5150c979b700f08
Chris Clampitt <christian.clampitt@nyu.edu>
2025-09-26 21:17:07 -0600

    Initial Commit

---
+++ StreamAnimation/StreamAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamAnimationApp.swift
+//  StreamAnimation
+//
+//  Created by Christian J Clampitt on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct StreamAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StreamAnimationTests/StreamAnimationTests.swift
@@ -0,0 +1,16 @@
+//
+//  StreamAnimationTests.swift
+//  StreamAnimationTests
+//
+//  Created by Christian J Clampitt on 9/26/25.
+//
+
+import Testing
+
+struct StreamAnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

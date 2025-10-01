1b0365f9786c346e414b9164d585142116747e1e
Chris Clampitt <christian.clampitt@nyu.edu>
2025-10-01 12:40:36 -0600

    memory test for Stitch

---
+++ BadAccessTest.swift
@@ -0,0 +1,16 @@
+//
+//  BadAccessTest.swift
+//  StreamAnimationTests
+//
+//  Created by Christian J Clampitt on 10/1/25.
+//
+
+import Testing
+
+struct BadAccessTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
